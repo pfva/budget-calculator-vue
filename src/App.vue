ac4d1e2bf2629340eb6f69d8a42e4e49ef9374e1
Unknown <petter.f.andersson@gmail.com>
2019-01-22 15:53:52 +0100

    Remove standard built-in component from Vue CLI

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,5 @@
 <template>
   <div id="app">
-    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
-    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
     <header class="container-fluid o-header">
       <SiteHeaderOrganism/>
     </header>
@@ -18,7 +16,6 @@
 </template>
 
 <script>
-// import HelloWorld from "./components/HelloWorld.vue";
 import SiteHeaderOrganism from "./components/organisms/SiteHeaderOrganism.vue";
 import IncomeOrganism from "./components/organisms/IncomeOrganism.vue";
 import ExpensesOrganism from "./components/organisms/ExpensesOrganism.vue";
@@ -27,7 +24,6 @@ import FooterOrganism from "./components/organisms/FooterOrganism.vue";
 export default {
   name: "app",
   components: {
-    // HelloWorld,
     SiteHeaderOrganism,
     IncomeOrganism,
     ExpensesOrganism,
@@ -38,12 +34,4 @@ export default {
 
 <style lang="scss">
 @import "../scss/main.scss";
-// #app {
-//   font-family: "Avenir", Helvetica, Arial, sans-serif;
-//   -webkit-font-smoothing: antialiased;
-//   -moz-osx-font-smoothing: grayscale;
-//   text-align: center;
-//   color: #2c3e50;
-//   margin-top: 60px;
-// }
 </style>
