1b6e83a7232686907808912090e66b9630f34ec4
Unknown <petter.f.andersson@gmail.com>
2019-01-28 16:59:02 +0100

    Add Chart with dummy data

--- src/components/organisms/ChartOrganism.vue
+++ src/components/organisms/ChartOrganism.vue
@@ -1,14 +1,148 @@
 <template>
   <div class="row justify-content-center">
     <div class="col-12 col-md-8 d-flex justify-content-center">
-      <svg class="o-chart o-chart--invisible-height"></svg>
+      <svg class="o-chart"></svg>
     </div>
   </div>
 </template>
 
 <script>
 import * as d3 from "d3";
-export default {};
+export default {
+  data() {
+    return {
+      expensesData: [
+        {
+          label: "Rent/Mortgage",
+          value: 10,
+          percentage: "10%"
+        },
+        {
+          label: "Electricity",
+          value: 20,
+          percentage: "20%"
+        },
+        {
+          label: "Heating/Water",
+          value: 30,
+          percentage: "30%"
+        },
+        {
+          label: "Food/Groceries",
+          value: 40,
+          percentage: "40%"
+        }
+      ]
+    };
+  },
+  mounted() {
+    this.drawChart();
+  },
+  methods: {
+    drawChart() {
+      let data = this.expensesData;
+      let chartEl = document.querySelector(".o-chart");
+      chartEl.innerHTML = "";
+      let chartStyles = window.getComputedStyle(chartEl);
+
+      let color = d3
+        .scaleOrdinal()
+        .domain(data.map(d => d.name))
+        .range(
+          d3.quantize(t => d3.interpolateSpectral(t * 0.9), data.length + 1)
+        );
+
+      let width = parseInt(chartStyles.width);
+      let height = parseInt(chartStyles.height);
+      let radius = Math.min(width, height) / 2;
+
+      let pie = d3
+        .pie()
+        .sort(null)
+        .value(d => d.value);
+
+      let arcs = pie(data);
+
+      let arc = d3
+        .arc()
+        .innerRadius(0)
+        .outerRadius(radius);
+
+      let svg = d3.select("svg");
+
+      let g = svg
+        .append("g")
+        .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+      let pathIndex = 0;
+      g.selectAll("path")
+        .data(arcs)
+        .enter()
+        .append("path")
+        .attr("fill", (d, i) => color(i))
+        .attr("stroke", "lightgray")
+        .attr("d", arc)
+        .attr("data-key", function(i) {
+          i = pathIndex++;
+          return i;
+        });
+
+      let textIndex = 0;
+      let text = g
+        .selectAll("text")
+        .data(arcs)
+        .enter()
+        .append("text")
+        .attr("transform", function(d) {
+          d.innerRadius = 0;
+          d.outerRadius = radius;
+          let c = arc.centroid(d);
+          return "translate(" + c[0] * 1.4 + "," + c[1] * 1.4 + ")";
+        })
+        .attr("data-key", function(i) {
+          i = textIndex++;
+          return i;
+        });
+
+      text
+        .append("tspan")
+        .attr("text-anchor", "middle")
+        .attr("x", 0)
+        .attr("y", "-0.7em")
+        .text((d, i) => data[i].label)
+        .append("tspan")
+        .attr("x", 0)
+        .attr("y", "0.7em")
+        .text(d => d.data.percentage);
+
+      let oChart = document.querySelector(".o-chart");
+      let oChartPaths = oChart.querySelectorAll("path");
+      let oChartTexts = oChart.querySelectorAll("text");
+
+      for (let k = 0; k < oChartPaths.length; k++) {
+        oChartPaths[k].addEventListener("mouseenter", function() {
+          for (let l = 0; l < oChartTexts.length; l++) {
+            if (oChartTexts[l].dataset.key !== oChartPaths[k].dataset.key) {
+              oChartTexts[l].classList.add("u-invisible");
+            }
+          }
+        });
+        oChartPaths[k].addEventListener("click", function() {
+          for (let l = 0; l < oChartTexts.length; l++) {
+            if (oChartTexts[l].dataset.key !== oChartPaths[k].dataset.key) {
+              oChartTexts[l].classList.toggle("u-invisible");
+            }
+          }
+        });
+        oChartPaths[k].addEventListener("mouseleave", function() {
+          for (let m = 0; m < oChartTexts.length; m++) {
+            oChartTexts[m].classList.remove("u-invisible");
+          }
+        });
+      }
+    }
+  }
+};
 </script>
 
 <style lang="scss">
