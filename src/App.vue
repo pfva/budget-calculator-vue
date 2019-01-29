c54c01d5935cea2f644bc2e45209c6a9f8a190a1
Unknown <petter.f.andersson@gmail.com>
2019-01-29 13:59:30 +0100

    Revert "Enable Chart to work with dynamic data, and re-render on data change"
    This reverts commit 71cb730c01919c22729c4ef7ce2cd341d450806f.

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@
       <IncomeOrganism v-on:totalIncome="addToIncome"/>
       <ExpensesOrganism v-on:totalExpenses="addToExpenses"/>
       <CalculateOrganism v-bind:sum="incomeMinusExpenses" v-on:clickEvent="handleClickEvent"/>
-      <ChartOrganism v-bind:showChart="showChart" v-bind:expenses="expenses"/>
+      <ChartOrganism v-bind:showChart="showChart"/>
     </main>
 
     <footer class="container-fluid footer o-footer">
@@ -53,7 +53,7 @@ export default {
       this.calculateIncomeMinusExpenses();
     },
     calculateIncomeMinusExpenses() {
-      this.incomeMinusExpenses = this.income - this.expenses[0];
+      this.incomeMinusExpenses = this.income - this.expenses;
     },
     handleClickEvent() {
       this.showChart = true;
--- src/components/organisms/ExpensesOrganism.vue
+++ src/components/organisms/ExpensesOrganism.vue
@@ -111,7 +111,7 @@ export default {
         summedValues += object.value;
       }
       this.totalValues = summedValues;
-      this.$emit("totalExpenses", [this.totalValues, this.payload]);
+      this.$emit("totalExpenses", this.totalValues);
     }
   }
 };
